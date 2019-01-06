aba6ab827252f1ef44e6a89d629f2f35875e80da
Bimal <timilsina.bml05@gmail.com>
2019-01-06 23:12:58 +0545

    Bimal Added

---
+++ app/src/main/java/com/example/prati/kachhya/home_page.java
@@ -0,0 +1,13 @@
+package com.example.prati.kachhya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
